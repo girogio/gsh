35843789a93eace9ba61b7469577769bfd4d7345
girogio <giorgiogrigolo03@gmail.com>
2022-06-30 19:37:29 +0200

    updated test

--- tests/tokenizer_test.c
+++ tests/tokenizer_test.c
@@ -1,16 +1,13 @@
 #include "../src/better_tokenizer.h"
 
-int main(void) {
+int main(int argc, char **argv) {
 
-  char *line;
-
-  line = strdup("ls -la | wc -l | figlet");
+  char *line = strdup(argv[1]);
 
   Args *tokenized_line = tokenize(line);
   char ***pipeline = tokenize_pipeline(tokenized_line->args);
 
   print_args(tokenized_line);
-
   free_pipeline_tokens(pipeline);
   free_args(tokenized_line);
   free(line);
